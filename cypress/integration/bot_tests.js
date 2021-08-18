c96f54c1d02a8f5a51ed84f02c36e00f48d2c5f3
Mikołaj Cięszczyk <mikolaj.cieszczyk@gmail.com>
2021-08-18 13:17:29 +0200

    tests added

---
+++ cypress/integration/bot_tests.js
@@ -0,0 +1,35 @@
+describe('Bot testing', () => {
+    const openWebpage = () => {
+        cy.visit('https://bot.dialogflow.com/e9b19ef3-d01a-4423-9d9d-017366afc942');
+    }
+
+    const getIframeDocument = () => {
+        return cy
+            .get('iframe')
+            .its('0.contentDocument').should('exist')
+    }
+
+    const getIframeBody = () => {
+        return getIframeDocument()
+            .its('body').should('not.be.undefined')
+            .then(cy.wrap)
+    }
+
+    it("Typing 'hej' and testing response", () => {
+        openWebpage();
+        getIframeBody().find('#query').type('hej').type('{enter}');
+        getIframeBody().find('.server-response').contains('hej co za klej');
+    });
+
+    it("Typing 'czesc' and testing response", () => {
+        openWebpage();
+        getIframeBody().find('#query').type('czesc').type('{enter}');
+        getIframeBody().find('.server-response').contains('czesc nie ma co jesc');
+    });
+
+    it("Typing 'witam' and testing response", () => {
+        openWebpage();
+        getIframeBody().find('#query').type('witam').type('{enter}');
+        getIframeBody().find('.server-response').contains('witam i o zdrowie pytam');
+    })
+})
